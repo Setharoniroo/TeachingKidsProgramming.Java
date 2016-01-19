378390e853a74eaeac015a237a77ba5b8f47b0e6
Setharoniroo <setharoniroo@gmail.com>
2016-01-19 11:28:01 -0600

    SimpleSquareQuiz Complete

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/DoubleLoop.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section01forloops/DoubleLoop.java
@@ -10,7 +10,9 @@ public class DoubleLoop
   {
     Tortoise.show();
     //    Set the tortoise x position to 225 --#1.1
+    Tortoise.setX(225);
     //    Set the tortoise y position to 150 --#1.2
+    Tortoise.setY(150);
     //    UPDATE the tortoise speed to 10 --#8.2
     Tortoise.setSpeed(5);
     //  
@@ -21,6 +23,7 @@ public class DoubleLoop
     //         Change the pen color of the line the tortoise draws to the next color on the Color Wheel --#5
     //         Move the tortoise 4 times the current line number you are drawing --#4
     //         Turn the tortoise 1/6 of 360 degrees to the left --#2
+    Tortoise.turn(-60);
     // 
     //         Do the following 15 times --(HINT: The new line number is j) --#7.1
     //              Set the pen width of the line the tortoise draws to 17 --#9
